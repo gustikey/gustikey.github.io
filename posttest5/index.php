ef8112f307ffd1bd9773bec03cc55090db694178
gustikey <muhammadgustikeyandu@gmail.com>
2022-10-19 22:10:55 +0800

    update

---
+++ posttest5/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Website Berita Sepak Bola</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header>
+            <h2>Berita Sepak Bola</h2>
+        </header>
+
+        <div class="list-table">
+            <h3>Daftar Login Website</h3>
+            <a href="formulir.php" class="tambah">Tambah Mahasiswa</a>
+            <table>
+                <tr class="thead">
+                    <th>No</th>
+                    <th nowrap>Nama Lengkap</th>
+                    <th>Email</th>
+                    <th>No Telpon</th>
+                    <th>Alamat</th>
+                    <th colspan="2">Actions</th>
+                </tr>
+                    <td class="hapus">
+                        <a href="hapus.php?id=">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                                <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                            </svg>
+                        </a>
+                    </td>
+                </tr>
+                
+                
+            </table>
+        </div>
+        
+    </body>
+</html>
